1d3b2e7510ae5ab656b75119ffe1a88942bcd498
tiziberto <bertorellotiziano@gmail.com>
2025-10-01 15:23:22 -0300

    corregido

---
+++ src/main/java/ar/edu/iua/iw3/integration/cli2/model/ProductCli2JsonDeserializer.java
@@ -0,0 +1,69 @@
+package ar.edu.iua.iw3.integration.cli2.model;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import ar.edu.iua.iw3.model.business.BusinessException;
+import ar.edu.iua.iw3.model.business.ICategoryBusiness;
+import ar.edu.iua.iw3.model.business.NotFoundException;
+import ar.edu.iua.iw3.util.JsonUtiles;
+
+public class ProductCli2JsonDeserializer extends StdDeserializer<ProductCli2> {
+
+
+	protected ProductCli2JsonDeserializer(Class<?> vc) {
+		super(vc);
+	}
+
+	private ICategoryBusiness categoryBusiness;
+
+	public ProductCli2JsonDeserializer(Class<?> vc, ICategoryBusiness categoryBusiness) {
+		super(vc);
+		this.categoryBusiness = categoryBusiness;
+	}
+
+	@Override
+	public ProductCli2 deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JacksonException {
+		ProductCli2 r = new ProductCli2();
+		JsonNode node = jp.getCodec().readTree(jp);
+        
+        //Añadimos la fecha de vencimiento:
+        String expirationStr = JsonUtiles.getString(node, "expiration_date,vencimiento,expiry_date".split(","), null);
+        Date expirationDate= null;
+        if (expirationStr != null) {
+            try {
+                expirationDate = new SimpleDateFormat("yyyy-MM-dd").parse(expirationStr);
+            } catch (ParseException e) {
+                throw new IOException("Formato de fecha de vencimiento inválido. Se esperaba 'yyyy-MM-dd'");
+            }
+        } else {
+            throw new IOException("La fecha de vencimiento es obligatoria");
+        }
+
+		String productDesc = JsonUtiles.getString(node,
+				"product,description,product_description,product_name".split(","), null);
+		if (productDesc == null || productDesc.trim().isEmpty()) { // Controlar el nombre del producto, que exsita y no este vacio
+		    throw new IOException("El nombre del producto es obligatorio");
+		}
+		double price = JsonUtiles.getDouble(node, "product_price,price_product,price".split(","), 0);
+		boolean stock = JsonUtiles.getBoolean(node, "stock,in_stock".split(","), false);
+		r.setFechaVencimiento(expirationDate);
+		r.setProduct(productDesc);
+		r.setPrecio(price);
+		r.setStock(stock);
+		String categoryName = JsonUtiles.getString(node, "category,product_category,category_product".split(","), null);
+		if (categoryName != null) {
+			try {
+				r.setCategory(categoryBusiness.load(categoryName));
+			} catch (NotFoundException | BusinessException e) {
+			}
+		}
+		return r;
+	}
+
+}
